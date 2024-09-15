0d08e87f6bfdaadc3e1fe046d9ca15b5e6efdd7d
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-09-14 20:43:01 -0400

    Finish schema content for payment collection

--- db.mjs
+++ db.mjs
@@ -7,11 +7,18 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true},
     password: {type: String, required: true},
-    payment: [{}]
+    payment: [{type: mongoose.Schema.Types.ObjectId, ref: 'Payment'}]
 })
 
 const paymentSchema = mongoose.Schema({
-
+    paymentId: {type: String, required: true},
+    user: {type: String, required: true},
+    paymentType: {type: String, required: true},
+    comment: {type: String, required: true},
+    amount: {type: Number, required: true},
+    createdAt: {type: Date, required: true},
+    receiver: {type: String, required: true},
+    status: {type: String, required: true}
 })
 
 const User = mongoose.model('User', userSchema)
