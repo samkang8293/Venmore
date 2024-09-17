2b72339f74098df5d322bebb2b8b820fd0c58f2d
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-09-17 00:13:36 -0400

    Impl passport for user authentication and set up login

--- app.mjs
+++ app.mjs
@@ -4,6 +4,9 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import bcrypt from 'bcrypt'
 
 import { User, Payment } from './db.mjs'
 import './config.mjs'
@@ -13,9 +16,32 @@ const server = createServer(app)
 const io = new Server(server)
 
 app.use(express.json())
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cors())
 
 app.use(express.static('dist'))
+
+passport.use(new LocalStrategy(async (username, password, cb) => {
+    // access MongoDB to retrieve user information
+    const user = await User.find({username: username})
+    const pw = await User.find({username: username}, {'password': password})
+
+    // if the username doesn't exist, send an error message
+    if (!user) {
+        return done(null, false, { message: 'Incorrect username or password'})
+    }
+
+    // hash the password to check MongoDB's record of hashed password, adding salt value of 10
+    const hashedpw = bcrypt.hashSync(password, 10)
+
+    // if the password doesn't match, send error message
+    if (hashedpw !== pw) {
+        return done(null, false, {message: 'Incorrect username or password'})
+    }
+}))
+
+// add cookies session from passport somewhere for making sure user stays logged in
 
 app.get('/', (req, res) => {
     res.json()
