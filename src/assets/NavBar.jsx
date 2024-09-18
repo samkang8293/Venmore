9e54264075cf59ce7a02a831560c81871a9a57f0
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-09-18 17:24:49 -0400

    Retrieve user session and add func that indicates login

--- src/assets/NavBar.jsx
+++ src/assets/NavBar.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import '../App.css'
 
 const NavBar = () => {
+    const [user, setUser] = useState("")
+    // if I'm retrieving user info, I'd have to do something with sessions and mongo
+    useEffect(async () => {
+        try {
+            const res = await fetch('/login')
+            const status = await res.json()
+
+            if (status) {
+                setUser(status.user)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }, [])
     return (
         <>
             <div className="navbar">
+                <span className="">Venmore</span>
                 <ul className="menu">
-                    <li className="menuItem" id="home"></li>
-                    <li className="menuItem" id="payment"></li>
-                    <li className='menuItem' id="user"></li>
+                    <li className="menuItem" id="payment">Make Payment</li>
+                    {user ? (
+                    <li className='menuItem' id="user">{user.name}</li>
+                    ) : (
+                        <li className='menuItem' id="login">Login</li>
+                    )}
                 </ul>
             </div>
         </>
     )
 }
+// add user avatar in navbar
 
 export default NavBar
