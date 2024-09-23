23c2ef6798e02b85f1eccf439ea886cb34aef347
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-09-22 23:28:04 -0400

    Flesh out handleLogin func and add register btn

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -5,15 +5,38 @@ const Login = () => {
     const [password, setPassword] = useState("")
 
     const handleLogin = async (evt) => {
+        try {
+            const res = await fetch('/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                }),
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
+            })
+            const status = await res.json()
 
+            if (status.username === username && status.password === password) {
+                // need to figure out what to do after login confirm
+                alert("Login Success!")
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     return (
         <>
             <div className="login">
                 <h1>Log In</h1>
-                <input/>
+                <input type="text" name="username" placeholder="Username" onChange={(e) => {setUsername(e.target.value)}}/>
+                <input type="password" name="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
                 <button onClick={handleLogin}>Login</button>
+            </div>
+            <div className="register">
+                <h4>Register</h4>
             </div>
         </>
     )
