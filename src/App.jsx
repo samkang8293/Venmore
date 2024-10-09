e16696d0a61db167e83e66f2742f53d69e634282
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-10-08 21:45:12 -0400

    Import functions for retrieve user sess and redir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
@@ -9,12 +7,29 @@ import Home from './pages/Home.jsx'
 import Payment from './pages/Payment.jsx'
 
 function App() {
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {
+    const getUser = async (url) => {
+        try {
+            const res = await fetch(url)
+            const status = await res.json()
+
+            if (status) {
+                setUser(status.user)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    getUser('/login')
+}, [])
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/payment" element={<Payment />} />
