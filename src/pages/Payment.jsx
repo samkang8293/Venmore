67fc98b1e07c62d5906c578341887dbb63fd5d36
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-10-08 21:46:17 -0400

    Create init request send func for frontend

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,25 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
+import Search from '../components/Search'
 
 const Payment = () => {
-    const [users, setUsers] = useState([])
+    const socket = io()
+
+    const [user, setUser] = useState(null)
+    const [recipients, setRecipients] = useState([])
     const [amount, setAmount] = useState(0)
     const [comment, setComment] = useState(null)
+
+    useEffect(() => {
+        const getUser = async (url) => {
+            try {
+                const res = await fetch(url)
+                const status = await res.json()
+
+                setUser(status)
+            } catch(e) {
+                console.log(e)
+            }
+        }
+        getUser('/login')
+    }, [])
+
+    const handleRequest = async (evt) => {
+        const res = await fetch('/payment/request', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: user,
+                paymentType: 'Request',
+                comment: comment,
+                amount: amount,
+                receiver: recipients
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        const status = await res.json()
+
+        if (status) {
+            window.location.href('/')
+            alert("Request sent!")
+        }
+    }
 
     return (
         <>
             <div className="payment">
-                <div className="">
-                    <input type="text" name="search" placeholder="Username" />
-                </div>
+                {/* set setUsers property in SearchBar component */}
+                <Search />
                 <div className="">
                     <label forHTML="amount">$</label>
-                    <input type="number" name="amount" placeholder="Amount" />
+                    <input type="number" name="amount" placeholder="Amount" onChange={(e) => {setAmount(e.target.value)}}/>
                 </div>
                 <div className="">
-                    <input type="text" name="comment" placeholder="What's this for?" />
+                    <input type="text" name="comment" placeholder="What's this for?" onChange={(e) => {setComment(e.target.value)}}/>
                 </div>
                 <button>Pay</button>
-                <button>Request</button>
+                <button onClick={handleRequest}>Request</button>
             </div>
         </>
     )
